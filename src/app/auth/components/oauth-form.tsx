8ea9d45741dba2cf29a5f77dea65b5df2075293b
FelipeOP <feljpe000@gmail.com>
2024-10-16 17:26:35 -0500

    refactor: Fix sign-in error handling and add user existence check
    - Fix sign-in error handling in the signInWithEmailAndPassword function
    - Add user existence check in the callback route to prevent duplicate user creation
    - Update the UserInfoActions module to include a function for checking if a user exists by ID
    - Update the callback route to save user information and role after successful sign-in

--- src/app/auth/components/oauth-form.tsx
+++ src/app/auth/components/oauth-form.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/ui/icons";
 import { createSupabaseBrowserClient } from "@/lib/supabase/client";
-import React from "react";
 
 export default function OAuthForm() {
   const supabase = createSupabaseBrowserClient();
