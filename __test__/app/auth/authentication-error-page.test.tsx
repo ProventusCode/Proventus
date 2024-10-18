fddb6a21a9378d0752ed861008e43c20452ed212
FelipeOP <feljpe000@gmail.com>
2024-10-18 04:20:52 -0500

    fix: update test files path

--- __test__/app/auth/authentication-error-page.test.tsx
+++ __test__/app/auth/authentication-error-page.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import AuthenticationErrorPage from "../../../app/auth/fail/page";
+import AuthenticationErrorPage from "../../../src/app/auth/fail/page";
 
 describe("AuthenticationErrorPage", () => {
   it("should render the card title", () => {
